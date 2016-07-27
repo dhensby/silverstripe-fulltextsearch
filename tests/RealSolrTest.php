d14454b69e41e187347cc4ac1a7a6f621dc949a2
Daniel Hensby <dhensby@silverstripe.com>
2016-07-27 16:25:18 +0100

    lets see what happens

---
+++ tests/RealSolrTest.php
@@ -0,0 +1,42 @@
+<?php
+
+class RealSolrTest extends SapphireTest {
+
+    protected $usesDatabase = true;
+
+    public function setUpOnce() {
+        parent::setUpOnce();
+        Solr::configure_server(array(
+            'host' => defined('SOLR_SERVER') ? SOLR_SERVER : 'localhost',
+            'port' => defined('SOLR_PORT') ? SOLR_PORT : 8983,
+            'path' => defined('SOLR_PATH') ? SOLR_PATH : '/solr/',
+            'version' => defined('SOLR_VERSION') ? SOLR_VERSION : 3,
+
+            'indexstore' => array(
+                'mode' => defined('SOLR_MODE') ? SOLR_MODE : 'file',
+                'auth' => defined('SOLR_AUTH') ? SOLR_AUTH : null,
+
+                // Allow storing the solr index and config data in an arbitrary location,
+                // e.g. outside of the webroot
+                'path' => defined('SOLR_INDEXSTORE_PATH') ? SOLR_INDEXSTORE_PATH : BASE_PATH . '/.solr',
+                'remotepath' => defined('SOLR_REMOTE_PATH') ? SOLR_REMOTE_PATH : null
+            )
+        ));
+        $configureService = new Solr_Configure();
+        $configureService->run(new SS_HTTPRequest('GET', 'dev/tasks/Solr_Configure'));
+    }
+
+    public function testIndex() {
+
+    }
+
+}
+
+class RealSolrTest_Index extends SolrIndex {
+
+    public function init() {
+        $this->addClass('Page');
+        $this->addAllFulltextFields();
+    }
+
+}
